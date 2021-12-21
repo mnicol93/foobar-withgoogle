6b249bc3fdf8b8fd180443737b039b7b47a23944
Marc Nicolas Oliva <76608265+mnicol93@users.noreply.github.com>
2021-12-21 04:10:09 -0500

    Update solution.py

--- solution1/solution.py
+++ solution1/solution.py
@@ -1,12 +1,10 @@
 def solution(msg):
     letterVal = 0
     str = list(msg)
-    strReturn = ''
+    
     for i in range(len(msg)):
         letterVal = ord(msg[i]) - 97
         if letterVal >= 0 and letterVal < 26:
             str[i] = chr((25-letterVal)+97)                
         
-    return ("".join(strReturn))
-
-solution("vmxibkgrlm")
+    return ("".join(str))
